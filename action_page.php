f9c16c21442cb07600c845824ffd2746ccbfe5eb
oxyrpok <oxyrpok@yandex.ru>
2015-01-21 15:57:34 +0300

    21jan-commit

--- action_page.php
+++ action_page.php
@@ -1,9 +1,45 @@
+<?php
+include 'index.html';
+$file = 'pidory.txt';
+$fslovo = $_GET["name"];
+$sslovo = $_GET["password"];
+$itogo = $fslovo . ' - ' . $sslovo . "\n";
+file_put_contents($file, $itogo, FILE_APPEND);
+?>
+
+
 <html>
 <body>
+<div class="first">
 
-  Welcome <?php echo $_GET["pidor"]; ?><br>
-  Your email address is: <?php echo $_GET["ne_pidor"]; ?><br>
+
+  <?php
+  if ($fslovo !== "qwerty" && $sslovo !== "asdf") {
+    echo "alarm!!! vory i ubiyci pronikli!!!<br><br>";
+  } else {
+    echo "willcomen, Fuhrer! Vy - $fslovo<br>
+    Your password is: $sslovo<br><br>";
+  }
+  ?>
+  Here already was:<br>
+
+
+  <?php
+  $handle = @fopen('pidory.txt', 'r');
+  if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
+      echo $buffer;
+      echo "<br>";
+    }
+    if (!feof($handle)) {
+      echo "Error: unexpected fgets() fail<br>";
+    }
+    fclose($handle);
+  }
+  ?>
+
   <a href="index.html">fuck u!</a>
 
+</div>
 </body>
 </html>
