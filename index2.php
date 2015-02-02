30c9ccf60ff0d725f97bfd914f9b0c22576c4bfc
oxyrpok <oxyrpok@yandex.ru>
2015-02-02 05:25:40 -0800

    02feb2

---
+++ index2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>fuck</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="header">
+  eto header
+  </div>
+
+  <div class="left-menu">
+  eto left-menu
+  </div>
+
+  <div class="main">
+  eto main
+  </div>
+
+  <div class="right-menu">
+  eto right-menu
+  </div>
+  
+  <div class="footer">
+  eto footer
+  </div>
+
+</body>
+</html>
